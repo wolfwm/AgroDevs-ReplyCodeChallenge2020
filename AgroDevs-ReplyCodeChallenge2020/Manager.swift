d7c3b642b0f54707c8ad0ea6e5a75ec1cdf27aec
anascim <alexjnascim@gmail.com>
2020-03-12 14:19:53 -0300

    structure

---
+++ AgroDevs-ReplyCodeChallenge2020/Manager.swift
@@ -0,0 +1,11 @@
+//
+//  Manager.swift
+//  AgroDevs-ReplyCodeChallenge2020
+//
+//  Created by Alex Nascimento on 12/03/20.
+//  Copyright © 2020 AgroDevs. All rights reserved.
+//
+
+class Manager: Replyer {
+    
+}
