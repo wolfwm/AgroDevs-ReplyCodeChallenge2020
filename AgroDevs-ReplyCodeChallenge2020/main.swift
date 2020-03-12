8fe460d750bc8fc50704a61026e041de7d9a87e5
Wolfgang Walder <wolfwalder@gmail.com>
2020-03-12 13:12:34 -0300

    initial commit

---
+++ AgroDevs-ReplyCodeChallenge2020/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AgroDevs-ReplyCodeChallenge2020
+//
+//  Created by Wolfgang Walder on 12/03/20.
+//  Copyright © 2020 AgroDevs. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
