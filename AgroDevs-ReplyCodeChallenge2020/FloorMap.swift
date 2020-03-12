ec447cfdb75f8ff190a4a112f73005db4672a0f8
Wolfgang Walder <wolfwalder@gmail.com>
2020-03-12 16:27:00 -0300

    incomplete algorithm

--- AgroDevs-ReplyCodeChallenge2020/FloorMap.swift
+++ AgroDevs-ReplyCodeChallenge2020/FloorMap.swift
@@ -18,8 +18,9 @@ class FloorMap {
         var numNeighborManagerDesk = 0
         
         for i in (0..<matrix.count) {
+            deskMatrix.append([])
             for j in (0..<matrix[i].count) {
-                if let up = matrix[i+1][j] as Character? {
+                if let up = matrix[i][j-1] as Character? {
                     switch up {
                     case "#":
                         numNeighborUnavailble += 1
@@ -28,11 +29,11 @@ class FloorMap {
                     case "M":
                         numNeighborManagerDesk += 1
                     default:
-                        fatalError("Switch exausted")
+                        return
                     }
                 }
                 
-                if let down = matrix[i-1][j] as Character? {
+                if let down = matrix[i][j+1] as Character? {
                     switch down {
                     case "#":
                         numNeighborUnavailble += 1
@@ -41,11 +42,11 @@ class FloorMap {
                     case "M":
                         numNeighborManagerDesk += 1
                     default:
-                        fatalError("Switch exausted")
+                        return
                     }
                 }
                 
-                if let right = matrix[i][j+1] as Character? {
+                if let right = matrix[i+1][j] as Character? {
                     switch right {
                     case "#":
                         numNeighborUnavailble += 1
@@ -54,11 +55,11 @@ class FloorMap {
                     case "M":
                         numNeighborManagerDesk += 1
                     default:
-                        fatalError("Switch exausted")
+                        return
                     }
                 }
                 
-                if let left = matrix[i][j-1] as Character? {
+                if let left = matrix[i-1][j] as Character? {
                     switch left {
                     case "#":
                         numNeighborUnavailble += 1
@@ -67,14 +68,91 @@ class FloorMap {
                     case "M":
                         numNeighborManagerDesk += 1
                     default:
-                        fatalError("Switch exausted")
+                        return
                     }
                 }
                 
-                self.deskMatrix[i][j] = Desk(deskType: matrix[i][j], numNeighUnavailble: numNeighborUnavailble, numNeighDeveloperDes: numNeighborDeveloperDesk, numNeighManagerDesk: numNeighborManagerDesk, coord: Coord(x: i, y: j))
+                self.deskMatrix[i].append(Desk(deskType: matrix[i][j], numNeighUnavailble: numNeighborUnavailble, numNeighDeveloperDes: numNeighborDeveloperDesk, numNeighManagerDesk: numNeighborManagerDesk, coord: Coord(x: i, y: j)))
             }
         }
         self.developers = devList
         self.managers = managerList
+    }
+    
+    func populateOffice() -> [[Replyer]] {
+        var replyerMatrix = [[Replyer]]()
+        
+        for deskRow in (0..<deskMatrix.count) {
+            replyerMatrix.append([])
+            for cell in (0..<deskMatrix[deskRow].count) {
+                replyerMatrix[cell].append(Replyer(company: "placeholder", bonus: 0))
+            }
+        }
+        
+        var placed: Bool = false
+        
+        for dev in developers {
+            placed = false
+            for i in (0..<deskMatrix.count) {
+                for j in (0..<deskMatrix[i].count) {
+                    if deskMatrix[i][j].deskType == "_" {
+                        dev.coord = deskMatrix[i][j].coord
+                        replyerMatrix[i][j] = dev
+                        placed = true
+                        break
+                    }
+                }
+                if placed {
+                    break
+                }
+            }
+        }
+        
+        for manager in managers {
+            placed = false
+            for i in (0..<deskMatrix.count) {
+                for j in (0..<deskMatrix[i].count) {
+                    if deskMatrix[i][j].deskType == "M" {
+                        manager.coord = deskMatrix[i][j].coord
+                        replyerMatrix[i][j] = manager
+                        placed = true
+                        break
+                    }
+                }
+                if placed {
+                    break
+                }
+            }
+        }
+        
+        return replyerMatrix
+    }
+    
+    func calcScore(replyerMatrix: [[Replyer]]) {
+        var thisReplyer = Replyer(company: "placeholder", bonus: 0)
+        var wp = 0
+        var bp = 0
+        var tp = 0
+        var score = 0
+        
+        for i in (0..<replyerMatrix.count) {
+            for j in (0..<replyerMatrix[i].count) {
+                wp = 0
+                bp = 0
+                thisReplyer = replyerMatrix[i][j]
+                
+                if let right = replyerMatrix[i][j+1] as Replyer? {
+                    if let thisDeveloper = thisReplyer as? Developer, let rightDeveloper = right as? Developer {
+                        wp = thisDeveloper.skills.intersection(rightDeveloper.skills).count * (thisDeveloper.skills.union(rightDeveloper.skills).count - thisDeveloper.skills.intersection(rightDeveloper.skills).count)
+                    }
+                }
+                
+                if let down = replyerMatrix[i+1][j] as Replyer? {
+                    if let thisDeveloper = thisReplyer as? Developer, let downDeveloper = down as? Developer {
+                        wp = thisDeveloper.skills.intersection(downDeveloper.skills).count * (thisDeveloper.skills.union(downDeveloper.skills).count - thisDeveloper.skills.intersection(downDeveloper.skills).count)
+                    }
+                }
+            }
+        }
     }
 }
